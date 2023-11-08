4b081dd11b8e536776cde7895dbddadba4c681fd
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-07 22:21:21 -0500

    metodos y menu para gestionar soldados

--- lab 12/VideoJuego.java
+++ lab 12/VideoJuego.java
@@ -354,3 +354,322 @@ public static int obtenerColumnaLlegada(int columnaActual, String direccion) {
     public static int Aleatorio(int min, int max) {
         return (int) (Math.random() * (max - min + 1)) + min;
     }
+
+     public static ArrayList<Soldado>[] gestionarEjercitos() {
+    ArrayList<Soldado>[] arregloEjercitos = (ArrayList<Soldado>[]) new ArrayList[2];
+    arregloEjercitos = generarEjercitos();
+    ArrayList<Soldado> ejercito1 = arregloEjercitos[0];
+    ArrayList<Soldado> ejercito2 = arregloEjercitos[1];
+    // Lógica para generar los ejércitos y mostrar sus datos
+    mostrarDatosEjercitos(arregloEjercitos);
+    Scanner scanner = new Scanner(System.in);
+    boolean salir = false;
+    while (!salir) {
+        System.out.println("Escoja el ejército que desea gestionar:");
+        System.out.println("1. Ejército 1");
+        System.out.println("2. Ejército 2");
+        System.out.println("3. Volver");
+        int opcion = scanner.nextInt();
+        int juegaLuegoDeGestionar=0;
+        switch (opcion) {
+            case 1:
+                juegaLuegoDeGestionar=gestionarEjercito(ejercito1);
+                
+                if (juegaLuegoDeGestionar==10)
+                    codigoJuegoRapido(ejercito1,ejercito2);
+                break;
+            case 2:
+                gestionarEjercito(ejercito2);
+                if (juegaLuegoDeGestionar==10)
+                    codigoJuegoRapido(ejercito1,ejercito2);
+                break;
+            case 3:
+                salir = true;
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                break;
+        }
+    }
+    ArrayList<Soldado>[] ejercitos = new ArrayList[2];
+    ejercitos[0] = ejercito1;
+    ejercitos[1] = ejercito2;
+    return ejercitos;
+}
+    
+  
+
+    public static int gestionarEjercito(ArrayList<Soldado> ejercito) {
+    Scanner scanner = new Scanner(System.in);
+    boolean salir = false;
+    int opcion = 0;
+    while (!salir) {
+        System.out.println("Escoja una opción:");
+        System.out.println("1. Crear Soldado");
+        System.out.println("2. Eliminar Soldado");
+        System.out.println("3. Clonar Soldado");
+        System.out.println("4. Modificar Soldado");
+        System.out.println("5. Comparar Soldados");
+        System.out.println("6. Intercambiar Soldados");
+        System.out.println("7. Ver soldado");
+        System.out.println("8. Ver ejército");
+        System.out.println("9. Sumar niveles");
+        System.out.println("10. Jugar");
+        System.out.println("11. Volver");
+        opcion = scanner.nextInt();
+        switch (opcion) {
+             case 1:
+                crearSoldado(ejercito);
+                break;
+            case 2:
+                eliminarSoldado(ejercito);
+                break;
+            case 3:
+                clonarSoldado(ejercito);
+                break;
+            case 4:
+                modificarSoldado(ejercito);
+                break;
+            case 5:
+                compararSoldados(ejercito);
+                break;
+            case 6:
+                intercambiarSoldados(ejercito);
+                break;
+            case 7:
+                verSoldado(ejercito);
+                break;
+            case 8:
+                verEjercito(ejercito);
+                break;
+            case 9:
+                sumarNiveles(ejercito);
+                break;
+            case 10:
+                salir = true;
+                break;
+            case 11:
+                salir = true;
+                break;
+            default:
+                System.out.println("Opción inválida");
+                break;
+        }
+    }
+    return opcion;
+}
+    
+    public static void crearSoldado(ArrayList<Soldado> ejercito) {
+    Scanner scanner = new Scanner(System.in);
+    if (ejercito.size() >= 10) {
+        System.out.println("El ejército ya tiene el máximo de soldados permitidos.");
+        return;
+    }
+    System.out.println("Ingrese los datos del nuevo soldado:");
+ 
+    System.out.print("Nivel de ataque: ");
+    int nivelAtaque = scanner.nextInt();
+    System.out.print("Nivel de defensa: ");
+    int nivelDefensa = scanner.nextInt();
+    System.out.print("Vida actual: ");
+    int vidaActual = scanner.nextInt();
+    System.out.print("Velocidad: ");
+    int velocidad = scanner.nextInt();
+    System.out.print("Actitud: ");
+    String actitud = scanner.nextLine();
+    System.out.print("Característica: ");
+    String caracteristica = scanner.next();
+    Soldado nuevoSoldado = new Soldado("Soldado"+(ejercito.size())+caracteristica,  nivelAtaque,  nivelDefensa,  vidaActual,  velocidad,  actitud,caracteristica);
+    
+    ejercito.add(nuevoSoldado);
+    System.out.println("Soldado creado exitosamente.");
+}
+
+
+public static void eliminarSoldado(ArrayList<Soldado> ejercito) {
+    if (ejercito.size() == 1) {
+        System.out.println("No se puede eliminar soldados. El ejército solo tiene un soldado.");
+        return;
+    }
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Soldados a eliminar:");
+    for (int i = 0; i < ejercito.size(); i++) {
+        System.out.println((i + 1) + ". " + ejercito.get(i).getNombre());
+    }
+    System.out.print("Seleccione el número del soldado a eliminar: ");
+    int opcion = scanner.nextInt();
+    if (opcion < 1 || opcion > ejercito.size()) {
+        System.out.println("Opción inválida. Por favor, seleccione un número válido.");
+        return;
+    }
+    ejercito.remove(opcion - 1);
+    System.out.println("Soldado eliminado exitosamente.");
+}
+
+public static void clonarSoldado(ArrayList<Soldado> ejercito) {
+    if (ejercito.size() >= 10) {
+        System.out.println("El ejército ya tiene el máximo de soldados permitidos.");
+        return;
+    }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Soldados disponibles:");
+    for (int i = 0; i < ejercito.size(); i++) {
+        System.out.println((i + 1) + ". " + ejercito.get(i).getNombre());
+    }
+    System.out.print("Seleccione el número del soldado que desea clonar: ");
+    int opcion = scanner.nextInt();
+    if (opcion < 1 || opcion > ejercito.size()) {
+        System.out.println("no existe esa opcion ");
+        return;
+    }
+    Soldado soldadoPrincipal = ejercito.get(opcion - 1);
+    Soldado soldadoClonado = new Soldado("Soldado"+(ejercito.size()+1)+soldadoPrincipal.getCaracteristica()+ "_clon", soldadoPrincipal.getNivelAtaque(), soldadoPrincipal.getNivelDefensa(), soldadoPrincipal.getVidaActual() ,soldadoPrincipal.getVelocidad(),soldadoPrincipal.getActitud() ,soldadoPrincipal.getCaracteristica());
+                                          
+    ejercito.add(soldadoClonado);
+    System.out.println("Soldado clonado ");
+}
+
+public static void modificarSoldado(ArrayList<Soldado> ejercito) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Soldados disponibles:");
+    for (int i = 0; i < ejercito.size(); i++) {
+        System.out.println((i + 1) + ". " + ejercito.get(i).getNombre());
+    }
+    System.out.print("Seleccione el número del soldado que desea modificar: ");
+    int opcion = scanner.nextInt();
+    if (opcion < 1 || opcion > ejercito.size()) {
+        System.out.println("Opción inválida. Por favor, seleccione un número válido.");
+        return;
+    }
+    Soldado soldado = ejercito.get(opcion - 1);
+    System.out.println("Datos actuales del soldado:");
+    System.out.println("Nivel de ataque: " + soldado.getNivelAtaque());
+    System.out.println("Nivel de defensa: " + soldado.getNivelDefensa());
+    System.out.println("Vida actual: " + soldado.getVidaActual());
+
+    // Submenú para cambiar los atributos
+    System.out.println("Seleccione el atributo que desea modificar:");
+    System.out.println("1. Nivel de ataque");
+    System.out.println("2. Nivel de defensa");
+    System.out.println("3. Vida actual");
+    int opcionAtributo = scanner.nextInt();
+
+    switch (opcionAtributo) {
+        case 1:
+            System.out.print("Nuevo nivel de ataque: ");
+            soldado.setNivelAtaque(scanner.nextInt());
+            break;
+        case 2:
+            System.out.print("Nuevo nivel de defensa: ");
+            soldado.setNivelDefensa(scanner.nextInt());
+            break;
+        case 3:
+            System.out.print("Nueva vida actual: ");
+            soldado.setVidaActual(scanner.nextInt());
+            break;
+        default:
+            System.out.println("Opción inválida. No se realizaron cambios.");
+            break;
+    }
+}
+
+public static void compararSoldados(ArrayList<Soldado> ejercito) {
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Soldados disponibles:");
+    for (int i = 0; i < ejercito.size(); i++) {
+        System.out.println((i + 1) + ". " + ejercito.get(i).getNombre());
+    }
+    System.out.print("Seleccione el número del primer soldado que desea comparar: ");
+    int opcion1 = scanner.nextInt();
+    System.out.print("Seleccione el número del segundo soldado que desea comparar: ");
+    int opcion2 = scanner.nextInt();
+    if (opcion1 < 1 || opcion1 > ejercito.size() || opcion2 < 1 || opcion2 > ejercito.size()) {
+        System.out.println("Opción inválida. Por favor, seleccione números válidos.");
+        return;
+    }
+    Soldado soldado1 = ejercito.get(opcion1 - 1);
+    Soldado soldado2 = ejercito.get(opcion2 - 1);
+    if (soldado1.equals(soldado2)) {
+        System.out.println("Los soldados son iguales.");
+    } else {
+        System.out.println("Los soldados son diferentes.");
+    }
+}
+
+public static void intercambiarSoldados(ArrayList<Soldado> ejercito) {
+    if (ejercito.size() < 2) {
+        System.out.println("El ejército no tiene suficientes soldados para realizar un intercambio.");
+        return;
+    }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Soldados disponibles:");
+    for (int i = 0; i < ejercito.size(); i++) {
+        System.out.println((i + 1) + ". " + ejercito.get(i).getNombre());
+    }
+    System.out.print("Seleccione el número del primer soldado que desea intercambiar: ");
+    int opcion1 = scanner.nextInt();
+    System.out.print("Seleccione el número del segundo soldado que desea intercambiar: ");
+    int opcion2 = scanner.nextInt();
+    if (opcion1 < 1 || opcion1 > ejercito.size() || opcion2 < 1 || opcion2 > ejercito.size()) {
+        System.out.println("Opción inválida. Por favor, seleccione números válidos.");
+        return;
+    }
+    Soldado soldado1 = ejercito.get(opcion1 - 1);
+    Soldado soldado2 = ejercito.get(opcion2 - 1);
+    ejercito.set(opcion1 - 1, soldado2);
+    ejercito.set(opcion2 - 1, soldado1);
+    System.out.println("los soldados fueron intercambiados");
+}
+
+public static void verSoldado(ArrayList<Soldado> ejercito) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el nombre del soldado que desea buscar: ");
+    String nombreSoldado = scanner.nextLine();
+    
+    Soldado soldadoEncontrado = null;
+    for (Soldado soldado : ejercito) {
+        if (soldado.getNombre().equalsIgnoreCase(nombreSoldado)) {
+            soldadoEncontrado = soldado;
+            break;
+        }
+    }
+    
+    if (soldadoEncontrado == null) {
+        System.out.println("Soldado no encontrado.");
+        return;
+    }
+    
+    System.out.println("Datos del soldado:");
+    System.out.println("Nombre: " + soldadoEncontrado.getNombre());
+    System.out.println("Nivel de ataque: " + soldadoEncontrado.getNivelAtaque());
+    System.out.println("Nivel de defensa: " + soldadoEncontrado.getNivelDefensa());
+    System.out.println("Vida actual: " + soldadoEncontrado.getVidaActual());
+    System.out.println("Velocidad: " + soldadoEncontrado.getVelocidad());
+    System.out.println("Característica: " + soldadoEncontrado.getCaracteristica());
+}
+
+public static void verEjercito(ArrayList<Soldado> ejercito) {
+   
+    System.out.println("Soldados del ejército:");
+    for (Soldado soldado : ejercito) {
+        System.out.println("Nombre: " + soldado.getNombre());
+        System.out.println("Nivel de ataque: " + soldado.getNivelAtaque());
+        System.out.println("Nivel de defensa: " + soldado.getNivelDefensa());
+        System.out.println("Vida actual: " + soldado.getVidaActual());
+        System.out.println("Velocidad: " + soldado.getVelocidad());
+        System.out.println("Característica: " + soldado.getCaracteristica());
+        System.out.println("------------------------");
+    }
+}
+
+public static Soldado sumarNiveles(ArrayList<Soldado> ejercito) {
+    Soldado soldadoSuma = new Soldado("SumaSoldado", 0, 0, 0, 0, "", ""); // Soldado inicial con valores 0
+    for (Soldado soldado : ejercito) {
+        soldadoSuma = soldadoSuma.sumar(soldado); // Su0mar cada soldado al soldadoSuma
+    }
+    return soldadoSuma;
+}
+
+}
