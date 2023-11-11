66d6df29192fbf02b7d79e7e9671bc678f212e95
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-11 17:31:10 -0500

    creando objeto mapa y obteniendo sus dos reinos en dos arraylist

--- lab 17/VideoJuego.java
+++ lab 17/VideoJuego.java
@@ -1,1 +1,11 @@
+import java.util.*;
 
+public class Lab16 {
+    public static void main(String[] args) {
+        Mapa mapa = new Mapa();
+       
+       ArrayList<Ejercito>[] reinos=  mapa.tableroBatalla();
+       ArrayList<Ejercito> reino1 =reinos[0];
+        ArrayList<Ejercito> reino2 =reinos[1];
+    }
+}
