2a2428b8a0677d20660a34ece1abd0c1e6b81a6f
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-11 13:38:44 -0500

    metodos auxiliares

--- lab 16/VideoJuego.java
+++ lab 16/VideoJuego.java
@@ -8,3 +8,25 @@ public class VideoJuego {
        ArrayList<Ejercito> reino1 =reinos[0];
         ArrayList<Ejercito> reino2 =reinos[1];
     }
+    private static int CantidadTotalSoldados(ArrayList<Ejercito> reino) {
+        int cantidadTotalSoldados = 0;
+        for (Ejercito ejercito : reino) {
+            cantidadTotalSoldados += ejercito.getCantidadSoldados();
+        }
+        return cantidadTotalSoldados;
+    }
+
+    
+    private static int SumatoriaNivelVida(ArrayList<Ejercito> reino) {
+        int sumatoriaNivelVida = 0;
+        for (Ejercito ejercito : reino) {
+            sumatoriaNivelVida += ejercito.getSumatoriaNivelVidaSoldados();
+        }
+        return sumatoriaNivelVida;
+    }
+
+    //porcentaje aleatorio entre 0 y 100
+    private static double PorcentajeAleatorio() {
+        return Math.random() * 100;
+    }
+}
