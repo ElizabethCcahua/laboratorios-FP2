277121dc1e33c7b573b0959f38d54b7ed761da22
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-19 13:09:37 -0500

    clase caballero heredada de soldado

---
+++ lab 19/Caballero.java
@@ -0,0 +1,42 @@
+class Caballero extends Soldado{
+    private boolean montado;
+    private String armaActual;
+
+    public Caballero() {
+        montado = false;
+        armaActual = "lanza";
+    }
+
+    public void cambiarArma() {
+        if (armaActual.equals("espada")) {
+            armaActual = "lanza";
+        } else {
+            armaActual = "espada";
+        }
+    }
+
+    public void desmontar() {
+        if (montado) {
+            montado = false;
+            armaActual = "espada";
+        }
+    }
+
+    public void montar() {
+        if (!montado) {
+            montado = true;
+            armaActual = "lanza";
+        }
+         envestir();
+    }
+
+//modificar-cambiar metodo
+    public void envestir() {
+        if (montado) {
+            System.out.println("ataca 3 veces!");
+        } else {
+            System.out.println("ataca 2 veces!");
+        }
+    }
+}
+
