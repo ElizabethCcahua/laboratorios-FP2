5c37c458036b276b337494df4cb826321b851910
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-26 10:22:53 -0500

    constantes de clase y  to string sobreescrito

--- lab 21/Caballero.java
+++ lab 21/Caballero.java
@@ -1,9 +1,15 @@
 class Caballero extends Soldado{
     private boolean montado;
     private String armaActual;
+      private static final int NIVEL_ATAQUE = 13;
+      private static final int NIVEL_DEFENSA = 7;
 
     public Caballero() {
         super();
+       nivelAtaque= NIVEL_ATAQUE ;
+       nivelDefensa=NIVEL_DEFENSA ;   
+       nivelVida=Soldado.Aleatorio(10, 12);
+       vidaActual=nivelVida;
         this.setNombre("Caballero " + (getContadorCaballero() + 1));
         aumentarContadorCaballero();
         montado = false;
@@ -40,5 +46,27 @@ class Caballero extends Soldado{
         } else {
             System.out.println("ataca 2 veces!");
         }
+    }
+     public void setMontado(boolean montado) {
+        this.montado = montado;
+    }
+
+    public boolean isMontado() {
+        return montado;
+    }
+
+    public void setArmaActual(String armaActual) {
+        this.armaActual = armaActual;
+    }
+
+    public String getArmaActual() {
+        return armaActual;
+    }
+
+     @Override
+    public String toString() {
+        return super.toString() +
+                "Montado: " + montado + "\n" +
+                "Arma actual: " + armaActual;
     }
 }
