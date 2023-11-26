5f73cafa6307785d532a519ee7451579e243f25d
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-26 06:56:05 -0500

    lab anterior

---
+++ lab 20/Arquero.java
@@ -0,0 +1,22 @@
+class Arquero extends Soldado{
+    private int numFlechas;
+    
+    public Arquero() {
+        super();
+        this.numFlechas = Aleatorio(1, 10);
+    }
+    
+    public Arquero(String nombre, int nivelAtaque, int nivelDefensa, int vidaActual, int velocidad, String actitud, String caracteristica, int numFlechas) {
+        super(nombre, nivelAtaque, nivelDefensa, vidaActual, velocidad, actitud, caracteristica);
+        this.numFlechas = numFlechas;
+    }
+    
+    // Métodos set y get atributo numFlechas
+    public void setNumFlechas(int numFlechas) {
+        this.numFlechas = numFlechas;
+    }
+    
+    public int getNumFlechas() {
+        return numFlechas;
+    }
+}
