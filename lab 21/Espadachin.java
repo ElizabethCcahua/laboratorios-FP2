f4c11170ba9db95fa30970f939f455b46103d4bd
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-26 10:02:50 -0500

    Espadachin lab anterior

---
+++ lab 21/Espadachin.java
@@ -0,0 +1,28 @@
+class Espadachin extends Soldado{
+    private int longitudEspada;
+    
+    public Espadachin() {
+        super();
+        this.setNombre("Espadachin " + (getContadorEspadachin() + 1));
+        aumentarContadorEspadachin();
+        this.longitudEspada = Aleatorio(1, 5);
+    }
+    
+    public Espadachin(String nombre, int nivelAtaque, int nivelDefensa, int vidaActual, int velocidad, String actitud, String caracteristica, int longitudEspada) {
+        super(nombre, nivelAtaque, nivelDefensa, vidaActual, velocidad, actitud, caracteristica);
+        this.longitudEspada = longitudEspada;
+    }
+    
+    public void crearMuroEscudos() {
+        // falta crear método 
+    }
+    
+    // Métodos set y get atributo longitudEspada
+    public void setLongitudEspada(int longitudEspada) {
+        this.longitudEspada = longitudEspada;
+    }
+    
+    public int getLongitudEspada() {
+        return longitudEspada;
+    }
+}
