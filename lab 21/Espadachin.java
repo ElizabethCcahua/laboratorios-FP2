b7409b19661561fb77108ed45b5a517222a4e779
ElizabethCcahua <144204084+ElizabethCcahua@users.noreply.github.com>
2023-11-26 10:25:42 -0500

    constantes de clase y tostring sobreescrito  con atributos particulares

--- lab 21/Espadachin.java
+++ lab 21/Espadachin.java
@@ -1,8 +1,14 @@
 class Espadachin extends Soldado{
     private int longitudEspada;
+      private static final int NIVEL_ATAQUE = 10;
+    private static final int NIVEL_DEFENSA = 8;
     
     public Espadachin() {
         super();
+        nivelAtaque= NIVEL_ATAQUE ;
+       nivelDefensa=NIVEL_DEFENSA ;
+       nivelVida=Soldado.Aleatorio(8, 10);
+       vidaActual=nivelVida;
         this.setNombre("Espadachin " + (getContadorEspadachin() + 1));
         aumentarContadorEspadachin();
         this.longitudEspada = Aleatorio(1, 5);
@@ -24,5 +30,10 @@ class Espadachin extends Soldado{
     
     public int getLongitudEspada() {
         return longitudEspada;
+    }
+     @Override
+    public String toString() {
+        return super.toString() +
+                "Longitud de la espada: " + longitudEspada;
     }
 }
